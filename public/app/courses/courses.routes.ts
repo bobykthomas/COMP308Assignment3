ccc995bf70114d7204dd840b8b209bb6057d47e4
bobykthomas <bthoma31@my.centennialcollege.ca>
2019-03-29 16:13:28 -0300

    Initial commit

---
+++ public/app/courses/courses.routes.ts
@@ -0,0 +1,18 @@
+﻿import { Routes } from '@angular/router';
+import { CoursesComponent } from './courses.component';
+import { CreateComponent } from './create/create.component';
+import { ListComponent } from './list/list.component';
+import { ViewComponent } from './view/view.component';
+import { EditComponent } from './edit/edit.component';
+import { StudentsListComponent } from './students-list/students-list.component';
+export const CoursesRoutes: Routes = [{
+    path: 'courses',
+    component: CoursesComponent,
+    children: [
+        { path: '', component: ListComponent },
+        { path: 'create', component: CreateComponent },
+        { path: ':courseId', component: ViewComponent },
+        { path: ':courseId/edit', component: EditComponent },
+        { path: 'students-list', component: StudentsListComponent }
+    ],
+}];
